32fcd980aaafae16292ee2cd55c9f4460cb002d7
evoea005 <evoea005@osloskolen.no>
2024-04-17 08:58:36 +0200

    hash password

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -1,22 +1,34 @@
 import { con } from "./connect";
 
+async function sha256(input) {
+	const inputbuffer = new TextEncoder().encode(input);
+	const hashbuffer = await crypto.subtle.digest("SHA-256", inputbuffer);
+
+	const hasharray = Array.from(new Uint8Array(hashbuffer));
+
+	const hash = hasharray.map((byte) => ("00" + byte.toString(16)).slice(-2)).join("");
+
+	return hash;
+}
 
 export const POST = async ({ request }) => {
 	const { username, password } = await request.json();
 
+	const passwordhash = await sha256(password);
 
 	// not working hahhahahaahaha
-	
-	// await fetch("http://localhost:4321/api/checkusername").then((res) => res.json()).then((data) => {
-	// 	if (data.found) {
-	// 		return new Response({ status: 200, body: { success: false } });
-	// 	}
-	// });
 
+	// await fetch("http://localhost:4321/api/checkusername")
+	// 	.then((res) => res.json())
+	// 	.then((data) => {
+	// 		if (data.found) {
+	// 			return new Response({ status: 200, body: { success: false } });
+	// 		}
+	// 	});
 
 	con.connect(function (err) {
 		if (err) throw err;
-		const sql = `INSERT INTO user (username, password) VALUES ('${username}', '${password}')`;
+		const sql = `INSERT INTO user (username, password) VALUES ('${username}', '${passwordhash}')`;
 		con.query(sql, function (err, result) {
 			if (err) throw err;
 			console.log(`user registered with id: ${result.insertId} and username: ${username}`);
@@ -24,4 +36,3 @@ export const POST = async ({ request }) => {
 	});
 	return new Response({ status: 200, body: { success: true } });
 };
-
